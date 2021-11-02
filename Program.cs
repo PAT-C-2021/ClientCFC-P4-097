d637645a2950b7a651850af1098955c789b43a44
ffadelm <faysalfadelm0107@gmail.com>
2021-11-02 16:40:29 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20190140097;
+
+namespace ClientCFC_P4_20190140097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("1 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+
+            //koordinat operation
+            Koordinat a = new Koordinat();
+            Koordinat b = new Koordinat();
+
+            a.X = 7;
+            a.Y = 8;
+
+            b.X = 5;
+            b.Y = 6;
+
+            double selisihX = a.X - b.X;
+            double selisihY = a.Y - b.Y;
+
+            double jarak = Math.Sqrt(Math.Pow(selisihX, 2) + Math.Pow(selisihY, 2));
+            Console.WriteLine("Hasil Koordinat " + jarak);
+            Console.ReadLine();
+        }
+    }
+}
